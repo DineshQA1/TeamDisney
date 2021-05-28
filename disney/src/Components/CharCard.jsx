f253566c7fb6f26913134d14fe46fc25931cb557
georgeryalls <gryalls@qa.com>
2021-05-28 14:06:28 +0100

    GR: Added toggle for extra info

--- disney/src/Components/CharCard.jsx
+++ disney/src/Components/CharCard.jsx
@@ -6,6 +6,9 @@ const CharCard = ({ character }) => {
     let position;
     let imgUrl;
 
+    var idIdent = "toggle" + character._id;
+    var idToggle = "#toggle" + character._id;
+
     if (character.imageUrl.includes(".png")) {
         position = strippedUrl.indexOf(".png");
         imgUrl = character.imageUrl.substring(0, position + 4);
@@ -21,12 +24,13 @@ const CharCard = ({ character }) => {
     return (
         <Card style={{ backgroundColor: "lightCyan", marginBottom: "1%", marginTop: "1%", marginRight: "1%", marginLeft: "1%" }}>
             <CardBody>
-                <img width="50%" src={imgUrl} alt="Card image cap" />
+                <img width="100" height="100" src={imgUrl} alt="Card image cap" />
                 <CardText><a href={character.sourceUrl} target="_blank" style={{ textDecoration: "none", color: "black", fontSize: "20px", fontWeight: "bold" }}>{character.name}</a></CardText>
-                    <Button color="primary" id="toggler" style={{ marginBottom: '1rem' }}> Show More Information</Button>
-                    <UncontrolledCollapse toggler="#toggler">
-                        <CardText>TV Shows: {character.tvShows}</CardText>
-                    </UncontrolledCollapse>
+                <Button buttoncolor="primary" id={idIdent} style={{ marginBottom: '1rem' }}> More Info</Button>
+                <UncontrolledCollapse toggler={idToggle}>
+                    {character.tvShows.length > 0 ? <CardText>TV Shows: {character.tvShows} </CardText> : null}
+                    {character.films.length > 0 ? <CardText>Films: {character.films} </CardText> : null}
+                </UncontrolledCollapse>
             </CardBody>
         </Card>
     )
